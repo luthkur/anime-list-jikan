cb89e6d9f7628e8bf5a55501732c33597a0066ad
Luthfi Kurniawan <luthkur@gmail.com>
2025-02-28 04:31:52 +0700

    feat: implement anime detail page stubs

---
+++ src/pages/anime/[id].tsx
@@ -0,0 +1,5 @@
+// TODO : add anime fetching in get server side prop, clientside fetching display all the data in the pages for making sure the fetching works.
+const Anime = () => {
+  return <div>Anime</div>;
+};
+export default Anime;
